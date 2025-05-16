bc2023d6171c306f45ea7fc96553e8db257fb1f5
Asif Ahammed <asifahammed359@gmail.com>
2025-05-16 22:42:07 +0600

    first commit

---
+++ app/seeker/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+function Seeker() {
+  return (
+    <SafeAreaView>
+        <View>
+            <Text>Seeker</Text>
+        </View>
+    </SafeAreaView>
+  )
+}
+
+export default Seeker;
